3868c5192291b341bd9c64eb49f9f0995fb3e358
Joy Bassey <joybassie909@gmail.com>
2022-12-08 13:52:16 -0800

    UNIX is basically a simple operating system, but you have to be a genius to understand the simplicity

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,17 +1,12 @@
 #include <stdio.h>
-#include <string.h>
-#include <unistd.h>
-/**
-* main - main block
-* Return: 1
+#include <unistd.h> 
+/** 
+* main - Entry
+* Return: Always 1
 */
 int main(void)
 {
-	int s = strlen("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
-
-	write(2,
-	 "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n",
-	 s);
+	write(1, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
 	return (1);
 }
 
