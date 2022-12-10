3be8712c5c90450d7d0c3b113ed4f14e47e76fd6
Joy Bassey <joybassie909@gmail.com>
2022-12-09 20:01:30 -0800

    alphABET

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -1,6 +1,6 @@
-/* 
+/*
 * File: 3-print_alphabets.c
-* Author: Joy Bassey 
+* Author: Joy Bassey
 */
 
 #include <stdio.h>
@@ -18,7 +18,7 @@ for (ch = 'a' ; ch <= 'z' ; ch++)
 putchar(ch);
 }
 for (ch = 'A' ; ch <= 'Z' ; ch++)
-{ 
+{
 putchar(ch);
 }
 putchar('\n');
