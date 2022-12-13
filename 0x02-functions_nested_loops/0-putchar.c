2a5f15e614a290944d7e609ad71b16e0c441aa90
Joy Bassey <joybassie909@gmail.com>
2022-12-13 13:32:15 -0800

    _putchar

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,12 +1,12 @@
 #include "main.h"
 
 /**
-* main - prints putchar
-* Return: always 0
-*/
+ * main - prints putchar
+ * Return: always 0
+ */
 int main(void)
 {
-	_putchar('_');
+	_putchar(");
 	_putchar('p');
 	_putchar('u');
 	_putchar('t');
