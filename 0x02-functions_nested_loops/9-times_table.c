4894038de96f627193d46c59204717227293615d
Joy Bassey <joybassie909@gmail.com>
2022-12-13 21:04:32 -0800

    Learn your times table

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -20,7 +20,8 @@ _putchar(',');
 _putchar(' ');
 _putchar(' ');
 _putchar(k + '0');
-} else if (k >= 10)
+}
+else if (k >= 10)
 {
 _putchar(',');
 _putchar(' ');
@@ -28,7 +29,7 @@ _putchar((k / 10) + '0');
 _putchar((k % 10) + '0');
 }
 }
- _putchar('\n');
+ putchar('\n');
 }
 }
 
