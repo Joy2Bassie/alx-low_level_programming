56e655069d95c22de58ad123a3ad5caf05f07017
Joy Bassey <joybassie909@gmail.com>
2022-12-13 21:58:18 -0800

    The World looks like a multiplication-table, or a mathematical equation

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -10,7 +10,7 @@ if (n >= 0 && n <= 15)
 {
 for (i = 0; i <= n; i++)
 {
- for (j = 0; j <= n; j++)
+for (j = 0; j <= n; j++)
 {
 k = j * i;
 if (j == 0)
@@ -39,7 +39,7 @@ _putchar(',');
 _putchar(' ');
 _putchar((k / 100) + '0');
 _putchar(((k / 10) % 10) + '0');
- _putchar((k % 10) + '0');
+_putchar((k % 10) + '0');
 }
 }
 _putchar('\n');
